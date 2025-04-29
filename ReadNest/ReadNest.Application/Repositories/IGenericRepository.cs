8059acca89f0c6e1a4d114a3ccc805f77fd6d6a1
21anhn <ltnanh21@gmail.com>
2025-04-29 11:54:19 +0700

    [Add] Add dependency, editorconfig, generic repository pattern

---
+++ ReadNest/ReadNest.Application/Repositories/IGenericRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace ReadNest.Application.Repositories
+{
+    /// <summary>
+    /// IGenericRepository
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <typeparam name="TKey"></typeparam>
+    public interface IGenericRepository<T, TKey> where T : class
+    {
+    }
+}
---
+++ ReadNest/ReadNest.Infrastructure/Persistence/Repositories/GenericRepository.cs
@@ -0,0 +1,13 @@
+﻿using ReadNest.Application.Repositories;
+
+namespace ReadNest.Infrastructure.Persistence.Repositories
+{
+    /// <summary>
+    /// GenericRepository
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <typeparam name="TKey"></typeparam>
+    public class GenericRepository<T, TKey> : IGenericRepository<T, TKey> where T : class
+    {
+    }
+}

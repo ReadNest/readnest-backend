9eb3c538e683892d6c3247a0fc67d40e287ad21b
21anhn <ltnanh21@gmail.com>
2025-04-30 22:39:42 +0700

    [Add] Add generic repository

--- ReadNest/ReadNest.Application/Repositories/IGenericRepository.cs
+++ ReadNest/ReadNest.Application/Repositories/IGenericRepository.cs
@@ -1,4 +1,6 @@
-﻿namespace ReadNest.Application.Repositories
+﻿using System.Linq.Expressions;
+
+namespace ReadNest.Application.Repositories
 {
     /// <summary>
     /// IGenericRepository
@@ -7,5 +9,24 @@
     /// <typeparam name="TKey"></typeparam>
     public interface IGenericRepository<T, TKey> where T : class
     {
+        Task<IEnumerable<T>> GetAllAsync(bool asNoTracking = true);
+        Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate, bool asNoTracking = true);
+        Task<T?> GetByIdAsync(TKey id);
+        Task<T> AddAsync(T entity);
+        Task<IEnumerable<T>> AddRangeAsync(IEnumerable<T> entities);
+        Task UpdateAsync(T entity);
+        Task UpdateRangeAsync(IEnumerable<T> entities);
+        Task DeleteAsync(T entity);
+        Task DeleteByIdAsync(TKey id);
+        Task DeleteRangeAsync(IEnumerable<T> entities);
+        Task<int> CountAsync();
+        Task<bool> ExistsAsync(Expression<Func<T, bool>> predicate);
+        void SaveChanges();
+        Task SaveChangesAsync();
+        Task<IEnumerable<T>> GetAllWithIncludeAsync(Func<IQueryable<T>, IQueryable<T>>? include = null, bool asNoTracking = true);
+        Task<IEnumerable<T>> FindWithIncludeAsync(Expression<Func<T, bool>> predicate, Func<IQueryable<T>, IQueryable<T>>? include = null, bool asNoTracking = true);
+        Task<IEnumerable<T>> GetPagedAsync(int pageNumber, int pageSize, bool asNoTracking = true);
+        Task<IEnumerable<T>> FindPagedAsync(Expression<Func<T, bool>> predicate, int pageNumber, int pageSize, bool asNoTracking = true);
+        Task<IEnumerable<T>> FindWithIncludePagedAsync(Expression<Func<T, bool>> predicate, Func<IQueryable<T>, IQueryable<T>>? include, int pageNumber, int pageSize, bool asNoTracking = true);
     }
 }
--- ReadNest/ReadNest.Infrastructure/Persistence/Repositories/GenericRepository.cs
+++ ReadNest/ReadNest.Infrastructure/Persistence/Repositories/GenericRepository.cs
@@ -1,4 +1,7 @@
-﻿using ReadNest.Application.Repositories;
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using ReadNest.Application.Repositories;
+using ReadNest.Infrastructure.Persistence.DBContext;
 
 namespace ReadNest.Infrastructure.Persistence.Repositories
 {
@@ -9,5 +12,166 @@ namespace ReadNest.Infrastructure.Persistence.Repositories
     /// <typeparam name="TKey"></typeparam>
     public class GenericRepository<T, TKey> : IGenericRepository<T, TKey> where T : class
     {
+        protected readonly AppDbContext _context;
+        protected readonly DbSet<T> _dbSet;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="context"></param>
+        public GenericRepository(AppDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync(bool asNoTracking = true)
+        {
+            IQueryable<T> query = _dbSet;
+            if (asNoTracking)
+                query = query.AsNoTracking();
+            return await query.ToListAsync();
+        }
+
+        public async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate, bool asNoTracking = true)
+        {
+            IQueryable<T> query = _dbSet.Where(predicate);
+            if (asNoTracking)
+                query = query.AsNoTracking();
+            return await query.ToListAsync();
+        }
+
+        public async Task<T?> GetByIdAsync(TKey id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            _ = await _dbSet.AddAsync(entity);
+            return entity;
+        }
+
+        public async Task<IEnumerable<T>> AddRangeAsync(IEnumerable<T> entities)
+        {
+            await _dbSet.AddRangeAsync(entities);
+            return entities;
+        }
+
+        public Task UpdateAsync(T entity)
+        {
+            _ = _dbSet.Update(entity);
+            return Task.CompletedTask;
+        }
+
+        public Task UpdateRangeAsync(IEnumerable<T> entities)
+        {
+            _dbSet.UpdateRange(entities);
+            return Task.CompletedTask;
+        }
+
+        public Task DeleteAsync(T entity)
+        {
+            _ = _dbSet.Remove(entity);
+            return Task.CompletedTask;
+        }
+
+        public async Task DeleteByIdAsync(TKey id)
+        {
+            var entity = await GetByIdAsync(id);
+            if (entity != null)
+            {
+                _ = _dbSet.Remove(entity);
+            }
+        }
+
+        public Task DeleteRangeAsync(IEnumerable<T> entities)
+        {
+            _dbSet.RemoveRange(entities);
+            return Task.CompletedTask;
+        }
+
+        public async Task<int> CountAsync()
+        {
+            return await _dbSet.CountAsync();
+        }
+
+        public async Task<bool> ExistsAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _dbSet.AnyAsync(predicate);
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            _ = await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<T>> GetAllWithIncludeAsync(Func<IQueryable<T>, IQueryable<T>>? include = null, bool asNoTracking = true)
+        {
+            IQueryable<T> query = _dbSet;
+            if (asNoTracking)
+                query = query.AsNoTracking();
+            if (include != null)
+                query = include(query);
+            return await query.ToListAsync();
+        }
+
+        public async Task<IEnumerable<T>> FindWithIncludeAsync(Expression<Func<T, bool>> predicate, Func<IQueryable<T>, IQueryable<T>>? include = null, bool asNoTracking = true)
+        {
+            IQueryable<T> query = _dbSet.Where(predicate);
+            if (asNoTracking)
+                query = query.AsNoTracking();
+            if (include != null)
+                query = include(query);
+            return await query.ToListAsync();
+        }
+
+        public void SaveChanges()
+        {
+            _ = _context.SaveChanges();
+        }
+
+        public async Task<IEnumerable<T>> GetPagedAsync(int pageNumber, int pageSize, bool asNoTracking = true)
+        {
+            IQueryable<T> query = _dbSet;
+            if (asNoTracking)
+                query = query.AsNoTracking();
+
+            return await query
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<T>> FindPagedAsync(Expression<Func<T, bool>> predicate, int pageNumber, int pageSize, bool asNoTracking = true)
+        {
+            IQueryable<T> query = _dbSet.Where(predicate);
+            if (asNoTracking)
+                query = query.AsNoTracking();
+
+            return await query
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<T>> FindWithIncludePagedAsync(
+            Expression<Func<T, bool>> predicate,
+            Func<IQueryable<T>, IQueryable<T>>? include,
+            int pageNumber,
+            int pageSize,
+            bool asNoTracking = true)
+        {
+            IQueryable<T> query = _dbSet.Where(predicate);
+            if (asNoTracking)
+                query = query.AsNoTracking();
+            if (include != null)
+                query = include(query);
+
+            return await query
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+        }
     }
 }

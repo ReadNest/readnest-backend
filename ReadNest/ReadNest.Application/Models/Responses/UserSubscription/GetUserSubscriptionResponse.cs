ea04a614c4b12e36f6ad84aba9a3fc5d46d41f38
21anhn <ltnanh21@gmail.com>
2025-07-13 17:00:15 +0700

    [Add] Add API get user subscription

---
+++ ReadNest/ReadNest.Application/Models/Responses/UserSubscription/GetUserSubscriptionResponse.cs
@@ -0,0 +1,6 @@
+﻿using ReadNest.Shared.Enums;
+
+namespace ReadNest.Application.Models.Responses.UserSubscription
+{
+    public record GetUserSubscriptionResponse(DateTime StartDate, DateTime? EndDate, string Status, string PackageName);
+}
---
+++ ReadNest/ReadNest.Application/UseCases/Interfaces/UserSubscription/IUserSubscriptionUseCase.cs
@@ -0,0 +1,10 @@
+﻿using ReadNest.Application.Models.Responses.UserSubscription;
+using ReadNest.Shared.Common;
+
+namespace ReadNest.Application.UseCases.Interfaces.UserSubscription
+{
+    public interface IUserSubscriptionUseCase
+    {
+        Task<ApiResponse<GetUserSubscriptionResponse>> GetUserSubscriptionByUserIdAsync(Guid userId);       
+    }
+}
